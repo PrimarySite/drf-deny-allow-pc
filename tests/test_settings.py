27b1810dcd0072d2639d5092edefc15d2bcf9153
Christian Ledermann <christian.ledermann@gmail.com>
2015-12-04 11:47:04 +0000

    remove unused code

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,19 +7,9 @@ DATABASES = {
 }
 
 SECRET_KEY = "django_tests_secret_key"
-#TIME_ZONE = 'UTC'
-#LANGUAGE_CODE = 'en'
-#ADMIN_MEDIA_PREFIX = '/static/admin/'
-#STATICFILES_DIRS = ()
-
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    #'django.contrib.sessions',
-    #'django.contrib.messages',
-    #'django.contrib.admin',
-    #'django.contrib.staticfiles',
-    #'django.contrib.sitemaps',
     'rest_framework',
 )
