5b1e1561a788bfad5a3a27cac49cd0c28bbe87da
Christian Ledermann <christian.ledermann@gmail.com>
2016-09-09 12:30:54 +0100

    isort cleanup

--- drfdapc/__init__.py
+++ drfdapc/__init__.py
@@ -5,8 +5,8 @@ from .permissions import DACrudBasePermission
 from .permissions import DARWBasePermission
 from .permissions import allow_all
 from .permissions import allow_authenticated
+from .permissions import allow_authorized_key
 from .permissions import allow_staff
 from .permissions import allow_superuser
 from .permissions import authenticated_users
 from .permissions import deny_all
-from .permissions import allow_authorized_key
--- runtests.py
+++ runtests.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os
+import sys
+
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_settings')
 sys.path.insert(0, 'tests')
 
-from django.core.management import call_command
 
 if __name__ == '__main__':
     import django
